3d0354e9a1afaf2ad44b8776f2c3d58379ebe88e
greeceball <griff33@gmail.com>
2020-06-16 16:40:17 -0600

    Working on PageViewController

--- ScoobyShotScoreKeeper/ScoobyShotScoreKeeper/Controllers/View Controllers/CustomPageViewController.swift
+++ ScoobyShotScoreKeeper/ScoobyShotScoreKeeper/Controllers/View Controllers/CustomPageViewController.swift
@@ -8,23 +8,47 @@
 
 import UIKit
 
-class CustomPageViewController: UIPageViewController {
-
+class CustomPageViewController: UIPageViewController, UIPageViewControllerDelegate, UIPageViewControllerDataSource {
+    
+    
+    
+    lazy var subViewControllers: [UIViewController] = {
+        return [
+            UIStoryboard(name: "MainMini", bundle: nil).instantiateViewController(identifier: "ScoreKeeperViewController") as! ScoreKeeperViewController
+        ]
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        self.delegate = self
+        self.dataSource = self
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func viewWillAppear(_ animated: Bool) {
+        self.navigationController?.setNavigationBarHidden(true, animated: true)
     }
-    */
-
+    
+    //MARK: - Data Source
+    func presentationCount(for pageViewController: UIPageViewController) -> Int {
+        return subViewControllers.count
+    }
+    
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
+        let currentIndex: Int = subViewControllers.firstIndex(of: viewController) ?? 0
+        if(currentIndex <= 0) {
+            return nil
+        }
+        return subViewControllers[currentIndex - 1]
+    }
+    
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController? {
+        
+        let currentIndex: Int = subViewControllers.firstIndex(of: viewController) ?? 0
+        if (currentIndex >= subViewControllers.count - 1) {
+            return nil
+        }
+        return subViewControllers[currentIndex + 1]
+        
+    }
 }
---
+++ ScoobyShotScoreKeeper/ScoobyShotScoreKeeper/Controllers/View Controllers/ScoreKeeperViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ScoreKeeperViewController.swift
+//  ScoobyShotScoreKeeper
+//
+//  Created by Colby Harris on 6/16/20.
+//  Copyright © 2020 Colby_Harris. All rights reserved.
+//
+
+import UIKit
+
+class ScoreKeeperViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
